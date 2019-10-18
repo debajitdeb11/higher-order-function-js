77297378be2e257e6130305f1d949a05f2b161e5
DEBAJIT DEB <debajitdeb11@gmail.com>
2019-10-18 08:40:30 +0530

    Create constructor.js

---
+++ constructor.js
@@ -0,0 +1,14 @@
+// This is the function which will take new entries.
+
+function HouseKeeper(name, age, place){
+  this.name = name;
+  this.age = age;
+  this.place = place;
+}
+
+// **************  Execution *************
+// for e.g.,
+
+// var houserkeeper1 = new Housekeeper ("Debajit", "22", "Silchar");    <<<<<--- this code will make a entry of housekeeper1 with the mentioned inputs.
+
+// var housekeeper2 = new Housekeeper ("Semikha", "26", "Jorhat");      <<<<<--- this code will make a entry of housekeeper2 with the mentioned inputs.
